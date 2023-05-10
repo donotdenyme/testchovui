ea6c38227cd00271b108e6b26bff40466ad4ec9b
donotdenyme <132136437+donotdenyme@users.noreply.github.com>
2023-05-10 13:44:36 +0300

    Add files via upload
    testchovui

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Nga Ngáo Ngơ';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100017126314179';
+const eventName = 'Happy Birthday';
+const titleCard = 'Happy Birthday';
+const contentCard = 'Chúc bạn có một ngày sinh nhật thật vui vẻ và ý nghĩa, tuổi mới nhiều niềm vui và sớm đạt được tham vọng :))';
+const giftImage = 'ngangaongo.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
